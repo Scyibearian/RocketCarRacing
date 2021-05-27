536a7549ecd0b037476fd17d047a69d54cc184a4
Scyibearian <cyberboy02@gmail.com>
2021-05-27 15:44:17 +0100

    3...2...1...GO!

---
+++ Rocket Car Racing/Assets/Scripts/CarControlActive.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+
+public class CarControlActive : MonoBehaviour
+{
+
+    public GameObject CarControl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CarControl.GetComponent<CarController>().enabled=true;
+    }
+
+}
